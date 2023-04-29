5a1d77904ce31d2a6ed640512176ccc7b2ac129a
Fedor <fedorgugnin22@gmail.com>
2023-04-29 19:00:23 +0300

    navigate to / after login with google

--- frontend/src/hocs/Layout.jsx
+++ frontend/src/hocs/Layout.jsx
@@ -2,22 +2,28 @@ import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import { getParameterByName } from "../helpers/getParameterByName";
 import { checkAuth, googleAuthenticate } from "../rtk/user";
-import { useDispatch } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const Layout = (props) => {
+  const navigate = useNavigate()
+  const { isAuthenticated } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const state = getParameterByName('state');
-  const code = getParameterByName('code'); //get code and state from google oauth2
+  const state = getParameterByName("state");
+  const code = getParameterByName("code"); //get code and state from google oauth2
   useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/")
+    }
     if (state && code) {
-      dispatch(googleAuthenticate({state, code}));
+      dispatch(googleAuthenticate({ state, code }));
     } else {
       const access = localStorage.getItem("access");
       if (access) {
         dispatch(checkAuth(access));
       }
     }
-  }, [location]);
+  }, [location, isAuthenticated]);
+
   return (
     <div>
       <NavBar />
