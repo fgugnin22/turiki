bcf6bda33b85d8be38c1a6ee1938eb92404d2707
Fedor <fedorgugnin22@gmail.com>
2023-05-24 14:59:30 +0300

    match page boilerplate

--- frontend/src/pages/Match.jsx
+++ frontend/src/pages/Match.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../hocs/Layout";
-
+import { tournamentAPI } from "../rtk/tournamentAPI";
 const Match = () => {
-  const params = useParams();
-  return (
-    <Layout>
-      <div>Match {params.id}</div>
-    </Layout>
-  );
+    const params = useParams();
+    const {data: match, isSuccess, isError} = tournamentAPI.useGetMatchByIdQuery({ id: params.id });
+    return (
+        <Layout>
+            <div>Match {isSuccess ? match.id : "loading or error"}</div>
+            <div>{isSuccess ? match.participants[0].team.name : "loading or error"}</div>
+            <div>{isSuccess ? match.participants[1].team.name : "loading or error"}</div>
+        </Layout>
+    );
 };
 
 export default Match;
