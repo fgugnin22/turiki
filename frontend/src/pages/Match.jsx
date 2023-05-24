0af0490f5643f2a3dce8170bc4fe7d5df726512d
Fedor <fedorgugnin22@gmail.com>
2023-05-24 23:48:25 +0300

    new chat ui

--- frontend/src/pages/Match.jsx
+++ frontend/src/pages/Match.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import Layout from "../hocs/Layout";
 import { tournamentAPI } from "../rtk/tournamentAPI";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+
 const Match = () => {
     const { user, isAuthenticated } = useSelector((state) => state.user);
     const params = useParams();
@@ -37,6 +38,11 @@ const Match = () => {
         });
         setMessage("");
     };
+    const messagesEndRef = useRef(null);
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+    useEffect(scrollToBottom, [messages]);
     return (
         <Layout>
             {isSuccess ? (
@@ -61,32 +67,59 @@ const Match = () => {
                 </div>
             ) : null}
             {isGetMessagesSuccess && isAuthenticated ? (
-                <div className="border-4 rounded-xl w-64 h-96 flex flex-col fixed top-20 right-0">
-                    <div className="w-full overflow-y-scroll">
+                <div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden min-h-[400px]">
+                    <div className="flex flex-col flex-grow h-0 p-4 overflow-auto">
                         {messages.map((message) => {
-                            let messageClass = "";
                             if (message.user === user?.name) {
-                                messageClass = " ml-auto";
-                            } else {
-                                messageClass = " mr-auto";
-                            }
-                            return (
-                                <div
-                                    className={`py-1 border-2 border-blue-300 px-2 rounded-md my-1 w-fit flex flex-row ${messageClass}`}
-                                    key={message.id}
-                                >
-                                    <div>
-                                        <p>{message.content}</p>{" "}
-                                        <p className=" text-xs font-light">
-                                            from {message.user}
-                                        </p>
+                                return (
+                                    <div
+                                        key={message.id}
+                                        className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end"
+                                    >
+                                        <div>
+                                            <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                                                <p className="text-sm">
+                                                    {message.content}
+                                                </p>
+                                            </div>
+                                            <span className="text-xs text-gray-500 leading-none">
+                                                2 min ago, you
+                                            </span>
+                                        </div>
+                                        <Link to={`/`}>
+                                            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                                        </Link>
                                     </div>
-                                </div>
-                            );
+                                );
+                            } else {
+                                return (
+                                    <div
+                                        key={message.id}
+                                        className="flex w-full mt-2 space-x-3 max-w-xs"
+                                    >
+                                        <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                                        <div>
+                                            <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                                                <p className="text-sm">
+                                                    {message.content}
+                                                </p>
+                                            </div>
+                                            <span className="text-xs text-gray-500 leading-none">
+                                                2 min ago,
+                                            </span>
+                                            <span className="text-xs text-gray-500 leading-none">
+                                                {" "}
+                                                {message.user}
+                                            </span>
+                                        </div>
+                                    </div>
+                                );
+                            }
                         })}
+                        <div ref={messagesEndRef}></div>
                     </div>
                     <form
-                        className="flex flex-row"
+                        className="flex flex-row bg-gray-300 p-3"
                         onSubmit={(e) => onSubmit(e)}
                     >
                         <input
@@ -97,9 +130,22 @@ const Match = () => {
                         />
                         <button
                             type="submit"
-                            className="w-[50px] mt-auto px-2 py-[6px] rounded-md my-1 bg-slate-200"
+                            className="w-[50px] px-2 py-1 my-1 rounded-md bg-slate-200 hover:bg-slate-50 transition-colors active:bg-slate-600"
                         >
-                            Send
+                            <svg
+                                className="w-[2em] h-[2em]"
+                                viewBox="0 0 20 20"
+                            >
+                                <path
+                                    className="w-[2em] h-[2em] fill-[#000]"
+                                    fill="none"
+                                    d="M1.729,9.212h14.656l-4.184-4.184c-0.307-0.306-0.307-0.801,0-1.107c0.305-0.306,0.801-0.306,1.106,0
+	l5.481,5.482c0.018,0.014,0.037,0.019,0.053,0.034c0.181,0.181,0.242,0.425,0.209,0.66c-0.004,0.038-0.012,0.071-0.021,0.109
+	c-0.028,0.098-0.075,0.188-0.143,0.271c-0.021,0.026-0.021,0.061-0.045,0.085c-0.015,0.016-0.034,0.02-0.051,0.033l-5.483,5.483
+	c-0.306,0.307-0.802,0.307-1.106,0c-0.307-0.305-0.307-0.801,0-1.105l4.184-4.185H1.729c-0.436,0-0.788-0.353-0.788-0.788
+	S1.293,9.212,1.729,9.212z"
+                                ></path>
+                            </svg>
                         </button>
                     </form>
                 </div>
