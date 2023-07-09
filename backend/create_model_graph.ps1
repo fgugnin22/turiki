819ccf099f58a9d6db6e059b474cf4b88c853157
Fedor <fedorgugnin22@gmail.com>
2023-07-09 15:22:48 +0300

    dockerized le backend

--- backend/create_model_graph.ps1
+++
@@ -1,4 +0,0 @@
-﻿Set-Location "C:\Users\fgugn\projects\proh\backend"
-venv/Scripts/activate
-py manage.py graph_models -a > models.dot
-dot -Tsvg models.dot > models.svg
--- backend/migrate.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location "C:\Users\fgugn\projects\proh\backend"
-venv/Scripts/activate
-python manage.py makemigrations
-python manage.py migrate
