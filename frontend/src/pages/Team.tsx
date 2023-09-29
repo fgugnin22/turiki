67f79d9b53626602d2ba5a369f1e3ebb59a0001c
Fedor <fedorgugnin22@gmail.com>
2023-09-29 20:25:44 +0300

    add tailwind ui kit  config

--- frontend/src/pages/Team.tsx
+++ frontend/src/pages/Team.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { useAppSelector } from "../shared/rtk/store";
 import { Layout } from "../processes/Layout";
 import { tournamentAPI } from "../shared/rtk/tournamentAPI";
+import FileInput from "../shared/FileUploadSingle";
+import FileUploadSingle from "../shared/FileUploadSingle";
 
 const Team = () => {
     const { userDetails: user, isAuthenticated } = useAppSelector(
@@ -31,48 +33,60 @@ const Team = () => {
                     Подать заявку на вступление
                 </button>
             )}
-            {isSuccess &&
-                data.players.map((player: any, i: number) => {
-                    if (player.team_status == "PENDING") {
-                        return (
-                            <p key={i} className="p-2 bg-lime-300 text-center">
-                                {player.name}
-                                <button
-                                    // teamId, userId, userName, status
-                                    onClick={() => {
-                                        acceptPlayerToTeam({
-                                            teamId: Number(params.id),
-                                            player_id: player.id
-                                        });
-                                    }}
-                                    className="p-2 rounded border-4 border-purple-500"
+            {isSuccess && (
+                <>
+                    {data.players.map((player: any, i: number) => {
+                        if (player.team_status == "PENDING") {
+                            return (
+                                <p
+                                    key={i}
+                                    className="p-2 bg-lime-300 text-center"
                                 >
-                                    Добавить в команду
-                                </button>
-                            </p>
-                        );
-                    } else {
-                        return (
-                            <p key={i} className="p-2 bg-slate-300 text-center">
-                                {player.name}
-                                {user?.team_status === "CAPTAIN" &&
-                                    user?.team === data?.id && (
-                                        <button
-                                            onClick={() => {
-                                                kickPlayerFromTeam({
-                                                    teamId: Number(params.id),
-                                                    player_id: player.id
-                                                });
-                                            }}
-                                            className="p-2 rounded border-4 border-red-500"
-                                        >
-                                            ВЫГНАТЬ НАХер!
-                                        </button>
-                                    )}
-                            </p>
-                        );
-                    }
-                })}
+                                    {player.name}
+                                    <button
+                                        // teamId, userId, userName, status
+                                        onClick={() => {
+                                            acceptPlayerToTeam({
+                                                teamId: Number(params.id),
+                                                player_id: player.id
+                                            });
+                                        }}
+                                        className="p-2 rounded border-4 border-purple-500"
+                                    >
+                                        Добавить в команду
+                                    </button>
+                                </p>
+                            );
+                        } else {
+                            return (
+                                <p
+                                    key={i}
+                                    className="p-2 bg-slate-300 text-center"
+                                >
+                                    {player.name}
+                                    {user?.team_status === "CAPTAIN" &&
+                                        user?.team === data?.id && (
+                                            <button
+                                                onClick={() => {
+                                                    kickPlayerFromTeam({
+                                                        teamId: Number(
+                                                            params.id
+                                                        ),
+                                                        player_id: player.id
+                                                    });
+                                                }}
+                                                className="p-2 rounded border-4 border-red-500"
+                                            >
+                                                ВЫГНАТЬ НАХер!
+                                            </button>
+                                        )}
+                                </p>
+                            );
+                        }
+                    })}
+                    <FileUploadSingle />
+                </>
+            )}
         </Layout>
     );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        fontFamily: {
-            roboto: ['"Roboto Flex"'] // Ensure fonts with spaces have " " surrounding it.
-        },
-        extend: {}
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  important: true,
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US"
+  },
+  theme: {
+    fontFamily: {
+      roboto: ['"Roboto Flex"'] // Ensure fonts with spaces have " " surrounding it.
     },
-    plugins: [require("tailwind-scrollbar")({ nocompatible: true })]
+    extend: {
+      backgroundImage: theme => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')"
+      })
+    }
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"]
+    }
+  },
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+  future: {
+    purgeLayersByDefault: true
+  }
 };
