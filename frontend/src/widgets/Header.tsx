d7770f6765d4594b58c3c3eb3f8345611ac3d330
Fedor <fedorgugnin22@gmail.com>
2024-01-06 15:28:06 +0300

    new header

--- frontend/src/widgets/Header.tsx
+++ frontend/src/widgets/Header.tsx
@@ -3,126 +3,145 @@ import { useAppDispatch, useAppSelector } from "../shared/rtk/store";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../app/RouteTypes";
 import { logout } from "../shared/rtk/user";
-
+const serverURL = import.meta.env.VITE_API_URL;
 const Header = () => {
-  const [isDropDownVisible, setIsDropDownVisible] = useState(false);
-  const dispatch = useAppDispatch();
-  const {
-    userDetails,
-    isAuthenticated,
-    userDetails: user
-  } = useAppSelector((state) => state.user);
-  return (
-    <div>
-      <nav className="bg-dark dark:bg-gray-800  shadow ">
-        <div className="px-8 mx-auto max-w-7xl">
-          <div className="flex items-center justify-between h-16">
-            <div className=" flex items-center">
-              <Link className="flex-shrink-0" to={ROUTES.TOURNAMENTS.path}>
-                {/* <img
-                  className="w-8 h-8"
-                  src="/icons/rocket.svg"
-                  alt="Workflow"
-                /> */}
+    const [isDropDownVisible, setIsDropDownVisible] = useState(false);
+    const dispatch = useAppDispatch();
+    const {
+        userDetails,
+        isAuthenticated,
+        userDetails: user
+    } = useAppSelector((state) => state.user);
+    return (
+        <header className="flex justify-between text-lightgray items-center h-[70px]">
+            <Link className="" to={ROUTES.TOURNAMENTS.path}>
                 <img
-                  width="40"
-                  height="40"
-                  src="https://img.icons8.com/fluency/48/vuejs.png"
-                  alt="логотип"
+                    width="40"
+                    height="40"
+                    src={`${serverURL}/assets/img/logo.svg`}
+                    alt="логотип"
+                    className="neonshadow"
                 />
-              </Link>
-              <div className="hidden md:block">
-                <div className="flex items-baseline ml-10 space-x-4">
-                  <Link
-                    className=" text-gray-800 focus:text-blue-700 focus:underline hover:text-blue-700 hover:underline w-24 transition ease-in duration-200 text-center text-sm font-normal"
+            </Link>
+            <div className="flex justify-between w-1/2 mt-[1px]">
+                <Link
+                    data-content="На главную"
+                    className=" text-lightgray text-lg 
+                    before:hover:bg-gradient-to-r
+                    before:hover:from-turquoise transition before:opacity-0 
+                    hover:before:opacity-100 before:hover:to-lightblue
+                     before:hover:bg-clip-text 
+                    hover:text-opacity-0 before:font-medium font-medium
+                    hover:before:content-[attr(data-content)] hover:relative
+                    hover:before:absolute neonshadow"
                     to={ROUTES.DASHBOARD.path}
-                  >
-                    На главную
-                  </Link>
-                </div>
-              </div>
-            </div>
-            {isAuthenticated && (
-              <>
-                {(!userDetails?.team ||
-                  userDetails?.team_status === "PENDING") && (
-                    <div className="flex ml-auto">
-                      <Link
-                        to={ROUTES.TEAMS.CREATE.path}
-                        className=" text-gray-800 focus:text-blue-700 focus:underline hover:text-blue-700 hover:underline w-24 transition ease-in duration-200 text-center text-sm font-normal"
-                        role="menuitem"
-                      >
-                        <span className="flex flex-col">
-                          Создать или найти команду
-                        </span>
-                      </Link>
-                    </div>
-                  )}
-
-                <div
-                  className={`flex ${!userDetails?.team || userDetails?.team_status === "PENDING"
-                      ? "ml-6"
-                      : "ml-auto"
-                    }`}
                 >
-                  {userDetails?.team &&
-                    userDetails?.team_status !== "PENDING" && (
-                      <Link
-                        to={ROUTES.TEAMS.TEAM_BY_ID.buildPath({
-                          id: userDetails?.team
-                        })}
-                        className="py-2 px-4 block ml-auto bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 mr-4 rounded-lg "
-                        role="menuitem"
-                      >
-                        <span className="flex flex-col">Моя команда</span>
-                      </Link>
-                    )}
-                  <Link
-                    to={ROUTES.DASHBOARD.path}
-                    className="py-2 pl-3 pr-2  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white min-w-24 transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-l-lg "
-                    role="menuitem"
-                  >
-                    <span className="flex flex-col">{userDetails?.name}</span>
-                  </Link>
-                  <button
-                    onClick={() => {
-                      dispatch(logout());
-                    }}
-                    className="py-2 pr-3 pl-2  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-24 transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-r-lg "
-                    role="menuitem"
-                  >
-                    Выйти
-                  </button>
-                </div>
-              </>
-            )}
-            <div className="block">
-              <div className="flex items-center ml-4 md:ml-6 ">
-                {!isAuthenticated && (
-                  <>
-                    <Link
-                      to={ROUTES.LOGIN.path}
-                      type="button"
-                      className="py-2 pl-3 pr-2  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-24 transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-l-lg "
-                    >
-                      Войти
-                    </Link>
-                    <Link
-                      to={ROUTES.REGISTER_ACCOUNT.path}
-                      type="button"
-                      className="py-2 pr-3 pl-2  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-44 transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-r-lg "
-                    >
-                      Зарегистрироваться
-                    </Link>
-                  </>
-                )}
-              </div>
+                    На главную
+                </Link>
             </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+            {isAuthenticated ? (
+                <>
+                    <button
+                        // to={ROUTES.DASHBOARD.path}
+                        className=" flex justify-between items-center gap-[20px] rounded-[10px] transition-colors hover:bg-turquoise hover:bg-opacity-20 py-2 px-3 relative"
+                        role="menuitem"
+                        onClick={() => setIsDropDownVisible((prev) => !prev)}
+                    >
+                        <img
+                            src={
+                                userDetails?.image
+                                    ? `${serverURL}/${userDetails.image}`
+                                    : `${serverURL}/assets/img/userdefaultloggedin.svg`
+                            }
+                            alt=""
+                            className=" rounded-full w-[45px] h-[45px]"
+                        />
+                        <span
+                            data-content={userDetails?.name}
+                            className="before:text-lg text-lg before:font-semibold before:bg-gradient-to-r before:from-turquoise before:bg-clip-text before:to-lightblue text-transparent
+                        before:absolute relative before:content-[attr(data-content)] neonshadow"
+                        >
+                            {userDetails?.name}
+                        </span>
+
+                        <img
+                            src={`${serverURL}/assets/img/accordion.svg`}
+                            alt=""
+                            className="neonshadow mt-[1px]"
+                        />
+                        {isDropDownVisible && (
+                            <div className="absolute flex flex-col -bottom-20 right-0 gap-1 transition">
+                                <button
+                                    onClick={() => {
+                                        dispatch(logout());
+                                    }}
+                                    className=" bg-lightblue p-1 rounded-lg text-xs"
+                                    role="menuitem"
+                                >
+                                    Мой профиль
+                                </button>
+                                <button
+                                    // onClick={() => {
+                                    //     dispatch(logout());
+                                    // }}
+                                    className=" bg-lightblue p-1 rounded-lg text-xs"
+                                    role="menuitem"
+                                >
+                                    Найти команду/моя команда
+                                </button>
+                                <button
+                                    onClick={() => {
+                                        dispatch(logout());
+                                    }}
+                                    className=" bg-lightblue p-1 rounded-lg text-xs"
+                                    role="menuitem"
+                                >
+                                    Выйти
+                                </button>
+                            </div>
+                        )}
+                    </button>
+                </>
+            ) : (
+                <div className="flex justify-between items-center gap-3">
+                    <Link
+                        className=" flex gap-[19px] justify-between items-center rounded-[10px] hover:bg-turquoise hover:bg-opacity-20 active:bg-turquoise active:bg-opacity-60 h-[52px] pl-5 pr-5 neonshadow transition-colors"
+                        to={ROUTES.LOGIN.path}
+                        type="button"
+                    >
+                        <img
+                            src={`${serverURL}/assets/img/defaultuser.svg`}
+                            alt=""
+                        />
+                        <span
+                            data-content="Вход"
+                            className="before:text-lg text-lg before:font-semibold before:bg-gradient-to-r before:from-turquoise before:bg-clip-text before:to-lightblue text-transparent
+                        before:absolute relative before:content-[attr(data-content)]"
+                        >
+                            Вход
+                        </span>
+                    </Link>
+                    <span
+                        data-content="/"
+                        className="before:text-lg neonshadow font-black before:bg-gradient-to-r before:from-turquoise before:bg-clip-text before:to-lightblue text-transparent
+                        before:absolute relative before:content-[attr(data-content)] neonshadow scale-150 mx-2 mb-1"
+                    >
+                        /
+                    </span>
+                    <Link
+                        to={ROUTES.REGISTER_ACCOUNT.path}
+                        type="button"
+                        data-content="Регистрация"
+                        className="before:text-lg text-lg before:font-semibold before:bg-gradient-to-r before:from-turquoise before:bg-clip-text before:to-lightblue text-transparent
+                        before:absolute relative before:content-[attr(data-content)] neonshadow rounded-[10px] hover:bg-turquoise hover:bg-opacity-20 h-[52px] px-4 flex items-center transition-colors
+                        active:bg-turquoise active:bg-opacity-60"
+                    >
+                        <span className="">Регистрация</span>
+                    </Link>
+                </div>
+            )}
+        </header>
+    );
 };
 
 export default Header;
--- frontend/src/widgets/RegistrationForm.tsx
+++ frontend/src/widgets/RegistrationForm.tsx
@@ -5,135 +5,122 @@ import { register } from "../shared/rtk/user";
 import { continueWithGoogle } from "../helpers/continueWithGoogle";
 import { ROUTES } from "../app/RouteTypes";
 const RegistrationForm = () => {
-  const dispatch = useAppDispatch();
-  const { isAuthenticated } = useAppSelector((state) => state.user);
-  const [accountCreated, setAccountCreated] = useState(false);
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    name: "",
-    re_password: ""
-  });
-  const { email, password, name, re_password } = formData;
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const target = e.target as HTMLInputElement;
-    return setFormData({ ...formData, [target.name]: target.value });
-  };
+    const dispatch = useAppDispatch();
+    const { isAuthenticated } = useAppSelector((state) => state.user);
+    const [accountCreated, setAccountCreated] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        name: "",
+        re_password: ""
+    });
+    const { email, password, name, re_password } = formData;
+    const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement;
+        return setFormData({ ...formData, [target.name]: target.value });
+    };
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (password === re_password) {
-      dispatch(register({ name, email, password, re_password }));
-      setAccountCreated(true);
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (password === re_password) {
+            dispatch(register({ name, email, password, re_password }));
+            setAccountCreated(true);
+        }
+    };
+    // check if authenticated -> redirect to homepage
+    const navigate = useNavigate();
+    if (isAuthenticated) {
+        navigate(ROUTES.DASHBOARD.path, { replace: true });
     }
-  };
-  // check if authenticated -> redirect to homepage
-  const navigate = useNavigate();
-  if (isAuthenticated) {
-    navigate(ROUTES.DASHBOARD.path, { replace: true });
-  }
-  if (accountCreated) {
-    navigate(ROUTES.LOGIN.path, { replace: true });
-  }
-  return (
-    <div className="flex flex-col max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-      <div className="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl dark:text-white">
-        Зарегистрироваться
-      </div>
-      <span className="justify-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
-        Уже есть аккаунт?
-        <Link
-          className="text-sm text-blue-500 underline hover:text-blue-700"
-          to={ROUTES.LOGIN.path}
-        >
-          Войти
-        </Link>
-      </span>
+    if (accountCreated) {
+        navigate(ROUTES.LOGIN.path, { replace: true });
+    }
+    return (
+        <div>
+            <div>Зарегистрироваться</div>
+            <span>
+                Уже есть аккаунт?
+                <Link to={ROUTES.LOGIN.path}>Войти</Link>
+            </span>
 
-      <div className="p-6 mt-8">
-        <form onSubmit={onSubmit}>
-          <div className="flex flex-col mb-2">
-            <div className=" relative ">
-              <input
-                type="text"
-                className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                name="name"
-                value={name}
-                onChange={(e: React.FormEvent<HTMLInputElement>) => onChange(e)}
-                placeholder="Ник"
-              />
-            </div>
-          </div>
-          <div className="flex flex-col mb-2">
-            <div className=" relative ">
-              <input
-                type="text"
-                onChange={(e: React.FormEvent<HTMLInputElement>) => onChange(e)}
-                name="email"
-                value={email}
-                id="create-account-email"
-                className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                placeholder="Почта"
-              />
-            </div>
-          </div>
-          <div className="flex gap-4 mb-2">
-            <div className=" relative ">
-              <input
-                type="password"
-                onChange={(e: React.FormEvent<HTMLInputElement>) => onChange(e)}
-                className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                name="password"
-                value={password}
-                placeholder="Пароль"
-                required
-              />
-            </div>
-            <div className=" relative ">
-              <input
-                type="password"
-                className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                name="re_password"
-                placeholder="Пароль еще раз"
-                value={re_password}
-                onChange={(e: React.FormEvent<HTMLInputElement>) => onChange(e)}
-                minLength={6}
-                required
-              />
-            </div>
-          </div>
+            <div>
+                <form onSubmit={onSubmit}>
+                    <div>
+                        <div>
+                            <input
+                                type="text"
+                                name="name"
+                                value={name}
+                                onChange={(
+                                    e: React.FormEvent<HTMLInputElement>
+                                ) => onChange(e)}
+                                placeholder="Ник"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <input
+                                type="text"
+                                onChange={(
+                                    e: React.FormEvent<HTMLInputElement>
+                                ) => onChange(e)}
+                                name="email"
+                                value={email}
+                                id="create-account-email"
+                                placeholder="Почта"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <input
+                                type="password"
+                                onChange={(
+                                    e: React.FormEvent<HTMLInputElement>
+                                ) => onChange(e)}
+                                name="password"
+                                value={password}
+                                placeholder="Пароль"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="password"
+                                name="re_password"
+                                placeholder="Пароль еще раз"
+                                value={re_password}
+                                onChange={(
+                                    e: React.FormEvent<HTMLInputElement>
+                                ) => onChange(e)}
+                                minLength={6}
+                                required
+                            />
+                        </div>
+                    </div>
 
-          <div className="flex w-full mb-2 mt-4">
-            <button
-              type="submit"
-              className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-            >
-              Создать аккаунт
-            </button>
-          </div>
-          <div className="flex gap-4 item-center">
-            <button
-              type="button"
-              onClick={continueWithGoogle}
-              className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-            >
-              <svg
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="mr-2"
-                viewBox="0 0 1792 1792"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
-              </svg>
-              Google
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+                    <div>
+                        <button type="submit">Создать аккаунт</button>
+                    </div>
+                    <div>
+                        <button type="button" onClick={continueWithGoogle}>
+                            <svg
+                                width="20"
+                                height="20"
+                                fill="currentColor"
+                                viewBox="0 0 1792 1792"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
+                            </svg>
+                            Google
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default RegistrationForm;
