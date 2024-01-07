078139d752032ec687e621975913a38af9cbb2de
Fedor <fedorgugnin22@gmail.com>
2024-01-07 23:05:53 +0300

    images folder

---
+++ ci.ps1
@@ -0,0 +1,10 @@
+#Для этого скрипта необходимо выполнить условия из Readme.md!!!
+Copy-Item  $pwd"\backend\dist\assets\img" -Destination $pwd -Recurse
+Remove-Item $pwd"\backend\dist" -Recurse
+cd $pwd"\frontend"
+npm i
+npm run build
+cd ..
+Copy-Item  $pwd"\frontend\dist" -Destination $pwd"\backend" -Recurse
+Copy-Item  $pwd"\img" -Destination $pwd"\backend\dist\assets" -Recurse
+Remove-Item $pwd"\img" -Recurse
