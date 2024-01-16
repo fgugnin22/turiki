f9d16852024883f207a78d6b1801ea568350df72
Fedor <fedorgugnin22@gmail.com>
2024-01-16 22:55:33 +0300

    404

--- frontend/src/pages/NoMatch.tsx
+++ frontend/src/pages/NoMatch.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { Layout } from "../processes/Layout";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../shared/RouteTypes";
 const NoMatch = () => {
