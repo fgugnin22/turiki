7e7aff085abad9c720cb6ffb4c18982fcbf28677
Fedor <fedorgugnin22@gmail.com>
2024-01-23 16:03:39 +0300

    lol

--- backend/turiki_app/consumers.py
+++ backend/turiki_app/consumers.py
@@ -1,37 +1,7 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
 from turiki_app.services import async_return_user, async_create_message
-import asyncio
-from contextlib import suppress
 
-
-
-# class Periodic:
-#     def __init__(self, func, time):
-#         self.func = func
-#         self.time = time
-#         self.is_started = False
-#         self._task = None
-
-#     async def start(self):
-#         if not self.is_started:
-#             self.is_started = True
-#             # Start task to call func periodically:
-#             self._task = asyncio.ensure_future(self._run())
-
-#     async def stop(self):
-#         if self.is_started:
-#             self.is_started = False
-#             # Stop task and await it stopped:
-#             self._task.cancel()
-#             with suppress(asyncio.CancelledError):
-#                 await self._task
-
-#     async def _run(self):
-#         while True:
-#             await asyncio.sleep(self.time)
-#             self.func()
-# consumers - аналог django views для websocket'ов
 class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         # Логика в момент подключения к websocket'у
