b4abb5ce785c6646ad6e3f46dee123a3bb14b457
fgugnin22 <fedorgugnin22@gmail.com>
2024-04-05 17:16:31 +0300

    activation fix

--- frontend/src/pages/Activate.tsx
+++ frontend/src/pages/Activate.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "../processes/Layout";
 import { Navigate, useParams } from "react-router-dom";
 import { activate } from "../shared/rtk/user";
@@ -6,42 +6,40 @@ import { useAppDispatch, useAppSelector } from "../shared/rtk/store";
 import { ROUTES } from "../shared/RouteTypes";
 import ButtonMain from "../shared/ButtonMain";
 const Activate = () => {
-    const dispatch = useAppDispatch();
-    const { isAuthenticated } = useAppSelector((state) => state.user);
-    const [verified, setVerified] = useState(false);
-    const params = useParams();
-    const verify_account = () => {
-        const uid = params.uid;
-        const token = params.token;
-        if (!(uid && token)) {
-            return;
-        } else {
-            dispatch(activate({ uid, token }));
-            setVerified(true);
-        }
-    };
-    // check if verified -> redirect to homepage
-    if (verified || isAuthenticated) {
-        return <Navigate to={ROUTES.DASHBOARD.path} replace />;
+  const dispatch = useAppDispatch();
+  const { isAuthenticated } = useAppSelector((state) => state.user);
+  const [verified, setVerified] = useState(false);
+  const params = useParams();
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const uid = params.uid;
+    const token = params.token;
+    if (!(uid && token)) {
+      return;
+    } else {
+      dispatch(activate({ uid, token }))
+        .then(() => setVerified(true))
+        .catch(() => {
+          setError(true);
+        });
     }
-    return (
-        <Layout>
-            <div className="flex justify-center mb-auto mt-auto">
-                <div className="">
-                    <ButtonMain
-                        className="py-12 px-24 focus:py-11 focus:px-[94px] active:py-11 active:px-[94px] rounded-xl text-3xl font-bold hover:scale-105 transition"
-                        onClick={verify_account}
-                        type="button"
-                        style={{
-                            marginTop: "50px"
-                        }}
-                    >
-                        Подтвердить почту
-                    </ButtonMain>
-                </div>
-            </div>
-        </Layout>
-    );
+  }, []);
+
+  if (verified || isAuthenticated) {
+    return <Navigate to={ROUTES.LOGIN.path} replace />;
+  }
+  return (
+    <Layout>
+      <div className="flex justify-center mb-auto mt-auto">
+        <div className="">
+          <h1 className="text-center text-lightblue text-4xl mt-24">
+            {error ? "Произошла ошибка...(" : "Подождите..."}
+          </h1>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Activate;
